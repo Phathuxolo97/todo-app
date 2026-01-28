1a070b278c357d2038b7b6ce15fc2962da423375
phathuxolo <phathuxolo.mabangula@gmail.com>
2026-01-28 21:17:11 +0200

    I moved the gui file from modules functions

--- gui.py
+++ gui.py
@@ -3,8 +3,8 @@ import FreeSimpleGUI as sg
 label1 = sg.Text("Enter todo: ")
 input_text = sg.InputText(tooltip="textbox")
 
-button = sg.button("Add")
+button = sg.Button("Add")
 
-window = sg.Window("My Todo App" , Layout=[[label1 , input_text , button]])
+window = sg.Window("My Todo App" , layout=[[label1 , input_text , button]])
 window.read()
 window.close()
